6abb6ed635d92d2080836c91bb98fdd8c72d69a5
jamie55 <1804288688@qq.com>
2019-06-24 00:02:12 +0800

    init commit

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+// // 处理异步请求
+// module.exports = {
+//     devServer: {
+//         proxy:
+//             {
+//             '/api':{
+//                 target:'http://www.liulongbin.top:3005'
+//             }
+//         }
+//     }
+// }
